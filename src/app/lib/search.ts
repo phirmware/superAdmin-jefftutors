24ea2ac79d80ffd4915869b39cd2ae6e3f239e62
phirmware <chibuzor.ojukwu@gmail.com>
2019-12-02 13:09:09 +0100

    fixed search issue with lower case

--- src/app/lib/search.ts
+++ src/app/lib/search.ts
@@ -1,6 +1,6 @@
 export const filterFunction = (element: any, content: string) => {
   element.forEach((x: any) => {
-    const shouldShow = x.textContent.indexOf(content) > -1;
+    const shouldShow = x.textContent.indexOf(content.toLowerCase()) > -1;
     x.style.display = shouldShow ? '' : 'none';
   });
 };
