3c857fe4f87c0e41a8361b4004d93e3e6a34fb4e
phirmware <chibuzor.ojukwu@gmail.com>
2020-08-06 20:26:44 +0100

    fixed error with appnav

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppnavComponent } from './components/appnav/appnav.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AppnavComponent],
-  exports: [AppnavComponent]
+  declarations: [],
 })
 export class SharedModule { }
